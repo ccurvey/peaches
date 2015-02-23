bd81f9476594c31efa7817bca3e54a389162b1bc
Chris Curvey <chris@chriscurvey.com>
2015-02-23 14:13:10 -0500

    added admin

--- hoops/games/admin.py
+++ hoops/games/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from games.models import School, Team, Game
 # Register your models here.
+admin.site.register(School)
+admin.site.register(Team)
+admin.site.register(Game)
